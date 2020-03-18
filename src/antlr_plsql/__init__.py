0b1ee53894015b99c3b7ff240d6064a7e21db1ea
cermatej <m.cermak17@gmail.com>
2020-03-18 17:56:17 +0100

    removed antlr-plsql files (added as submodules)

--- src/antlr_plsql/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "0.9.1"
-from . import antlr_py as grammar
-from . import ast
--- src/antlr_plsql/antlr_py/__init__.py
+++
@@ -1,4 +0,0 @@
-from .plsqlLexer import plsqlLexer as Lexer
-from .plsqlListener import plsqlListener as Listener
-from .plsqlParser import plsqlParser as Parser
-from .plsqlVisitor import plsqlVisitor as Visitor
