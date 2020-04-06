a00020cb3338c73847f855244a5f0beaa2348fa5
cermatej <m.cermak17@gmail.com>
2020-04-06 15:49:55 +0200

    add parse_time to es doc, mock exec_time for testing purposes

--- src/rest/parser_api/controllers/default_controller.py
+++ src/rest/parser_api/controllers/default_controller.py
@@ -26,16 +26,20 @@ def add_doc(body):  # noqa: E501
     if (not body.body or not body.index):
         return ApiResponse(status='Invalid input', tokens=None), 405
 
+    tick = datetime.now()
     try:
         te = TokenExtractor()
         tokens = te.analyse(body.body)
     except:
         return ApiResponse(status='Error while parsing query', tokens=None), 500
+    tock = datetime.now()
 
-    exec_time = randint(20, 1000) if random() < 0.8 else None # argument optional in api
+    exec_time = ((len(body.body) / 100) * randint(100,
+                                                  700)) if random() < 0.8 else None  # argument optional in api - mock for testing purposes
     doc = {
         'query': body.body,
         'tokens': tokens,
+        'parse_time': int((tock - tick).total_seconds() * 1000),
         'exec_time': exec_time,  # optional parameter - None if not provided
         'timestamp': datetime.now()  # optional parameter - if not provided use datetime.now()
     }
